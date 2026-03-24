b9726c74c92fbfc66c339ecdcf68704f1f9f71b6
hassib-95 <hassibbtc@gmail.com>
2026-03-24 19:37:29 +0000

    Fix EmailJS eval() issue for GitHub Pages compatibility

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,7 +1,5 @@
 // Initialize EmailJS with public key
-(function() {
-    emailjs.init("KUN1cq3bdTgISYea2");
-})();
+emailjs.init("KUN1cq3bdTgISYea2");
 
 // Get the form element
 const signupForm = document.getElementById('signup-form');
@@ -11,20 +9,8 @@ signupForm.addEventListener('submit', function(event) {
     // Prevent default form submission
     event.preventDefault();
     
-    // Get form values
-    const formData = {
-        first_name: document.getElementById('first_name').value,
-        last_name: document.getElementById('last_name').value,
-        email: document.getElementById('email_address').value,
-        gender: document.querySelector('input[name="gender"]:checked').value,
-        age_range: document.getElementById('age_range').value,
-        height: document.getElementById('height').value,
-        weight: document.getElementById('weight').value,
-        training_goal: document.querySelector('input[name="training_goal"]:checked').value
-    };
-    
-    // Send email using EmailJS
-    emailjs.send('service_dqirnrt', 'template_7oapgbm', formData)
+    // Send email directly from form using sendForm method (no eval)
+    emailjs.sendForm('service_dqirnrt', 'template_7oapgbm', this)
         .then(function(response) {
             // Success message
             alert('Thank you for signing up! We will contact you soon.');
